8a34384058b23a5fda7e40686519980df844a9d0
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2014-07-30 18:49:33 +0300

    Extend GitHubSimpleRepo

--- client/objects/GitHubRepo.php
+++ client/objects/GitHubRepo.php
@@ -4,7 +4,7 @@ require_once(__DIR__ . '/GitHubSimpleRepo.php');
 
 	
 
-class GitHubRepo extends GitHubObject
+class GitHubRepo extends GitHubSimpleRepo
 {
 	/* (non-PHPdoc)
 	 * @see GitHubObject::getAttributes()
