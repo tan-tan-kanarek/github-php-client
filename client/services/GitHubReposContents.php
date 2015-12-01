d7dacec6ba89e4915c89433200cc87e2be343457
Egogo Websolutions <info@egogo.nl>
2015-12-01 12:40:39 +0100

    Add getContents() function

--- client/services/GitHubReposContents.php
+++ client/services/GitHubReposContents.php
@@ -3,7 +3,8 @@
 require_once(__DIR__ . '/../GitHubClient.php');
 require_once(__DIR__ . '/../GitHubService.php');
 require_once(__DIR__ . '/../objects/GitHubReadmeContent.php');
-	
+require_once(__DIR__ . '/../objects/GitHubContents.php');
+    
 
 class GitHubReposContents extends GitHubService
 {
@@ -21,6 +22,21 @@ class GitHubReposContents extends GitHubService
 			$data['ref'] = $ref;
 		
 		return $this->client->request("/repos/$owner/$repo/readme", 'GET', $data, 200, 'GitHubReadmeContent');
+	}
+	
+	/**
+	 * Get the content
+	 * 
+	 * @param $ref string (Optional) - The String name of the Commit/Branch/Tag.  Defaults to `master`.
+	 * @return GitHubContent
+	 */
+	public function getContents($owner, $repo, $path, $ref = null)
+	{
+		$data = array();
+		if(!is_null($ref))
+			$data['ref'] = $ref;
+		
+		return $this->client->request("/repos/$owner/$repo/contents/$path", 'GET', $data, 200, 'GitHubContents');
 	}
 	
 }
