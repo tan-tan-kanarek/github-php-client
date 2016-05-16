a5ee1b661cf780ad4a4cba90c8442319f847fa7f
Joon-Yong Lee <joonyong.lee@pnnl.gov>
2016-05-16 13:34:26 -0700

    for functions to create/update/delete a file
    I tried to fill in some missing functions to create/update/delete a
    file, which is defined at
    https://developer.github.com/v3/repos/contents/.

--- client/services/GitHubReposContents.php
+++ client/services/GitHubReposContents.php
@@ -2,7 +2,8 @@
 
 require_once(__DIR__ . '/../GitHubClient.php');
 require_once(__DIR__ . '/../GitHubService.php');
-require_once(__DIR__ . '/../objects/GitHubReadmeContent.php');
+require_once(__DIR__ . '/../objects/GitHubRepoContent.php');
+require_once(__DIR__ . '/../objects/GitHubRepoContentCommit.php');
 	
 
 class GitHubReposContents extends GitHubService
@@ -20,8 +21,66 @@ class GitHubReposContents extends GitHubService
 		if(!is_null($ref))
 			$data['ref'] = $ref;
 		
-		return $this->client->request("/repos/$owner/$repo/readme", 'GET', $data, 200, 'GitHubReadmeContent');
+		return $this->client->request("/repos/$owner/$repo/readme", 'GET', $data, 200, 'GitHubRepoContent');
+	}
+
+	/**
+	 * Create a file
+	 * 
+	 * @param $path	string	Required. The content path.
+	 * @param $message	string	Required. The commit message.
+	 * @param $content	string	Required. The new file content, Base64 encoded.
+	 * @param $branch	string	The branch name. Default: the repository’s default branch (usually master)
+	 * @return GitHubRepoContentCommit
+	 */
+	public function createFile($owner, $repo, $path, $msg, $content, $branch='master')
+	{
+		$data = array();
+		$data['message'] = $msg;
+		$data['content'] = $content;
+		$data['branch'] = $branch;
+		
+		return $this->client->request("/repos/$owner/$repo/contents/$path", 'PUT', $data, 201, 'GitHubRepoContentCommit');
+	}
+
+	/**
+	 * Update a file
+	 * 
+	 * @param $path	string	Required. The content path.
+	 * @param $message	string	Required. The commit message.
+	 * @param $content	string	Required. The updated file content, Base64 encoded.
+	 * @param $sha	string	Required. The blob SHA of the file being replaced.
+	 * @param $branch	string	The branch name. Default: the repository’s default branch (usually master)
+	 * @return GitHubRepoContentCommit
+	 */
+	public function updateFile($owner, $repo, $path, $msg, $content, $sha, $branch='master')
+	{
+		$data = array();
+		$data['message'] = $msg;
+		$data['content'] = $content;
+		$data['ref'] = $branch;
+		$data['sha'] = $sha;
+
+		return $this->client->request("/repos/$owner/$repo/contents/$path", 'PUT', $data, 200, 'GitHubRepoContentCommit');
+	}
+
+	/**
+	 * Delete a file
+	 * 
+	 * @param $path	string	Required. The content path.
+	 * @param $message	string	Required. The commit message.
+	 * @param $sha	string	Required. The blob SHA of the file being replaced.
+	 * @param $branch	string	The branch name. Default: the repository’s default branch (usually master)
+	 * @return GitHubRepoContentCommit
+	 */
+	public function deleteFile($owner, $repo, $path, $msg, $content, $sha, $branch='master')
+	{
+		$data = array();
+		$data['message'] = $msg;
+		$data['branch'] = $branch;
+		$data['sha'] = $sha;
+
+		return $this->client->request("/repos/$owner/$repo/contents/$path", 'DELETE', $data, 200, 'GitHubRepoContentCommit');
 	}
 	
 }
-
